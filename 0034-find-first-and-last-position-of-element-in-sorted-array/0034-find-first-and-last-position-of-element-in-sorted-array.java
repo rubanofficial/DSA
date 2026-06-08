8663844e7cf55b9a8c4d6868f88871681be8fdf6
rubanofficial <rubans082005@gmail.com>
2026-06-08 14:50:14 +0530

    Time: 0 ms (100%), Space: 48.2 MB (49.32%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,20 +1,46 @@
 class Solution {
-    public int[] searchRange(int[] nums, int t) {
-        int []res = new int [2];
-        int s =-1,e=-1;
-        int c = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == t && c == 0){
-                s = i;
-                e = i;
-                c++;
+    int fres(int[] n, int t){
+        int l = 0;
+        int r = n.length-1;
+        int ans = -1;
+        while(l<=r){
+            int mid = (l + r)/2;
+            if(n[mid] == t){
+                ans = mid;
+                r = mid-1;
             }
-            if(nums[i] == t){
-                e = i;
+            else if(n[mid] > t){
+                r = mid-1;
+            }
+            else{
+                l = mid + 1;
             }
         }
-        res[0] = s;
-        res[1] = e;
+        return ans;
+    }
+    int sres(int[] n, int t){
+        int l = 0;
+        int r = n.length-1;
+        int ans = -1;
+        while(l<=r){
+            int mid = (l + r)/2;
+            if(n[mid] == t){
+                ans = mid;
+                l = mid+1;
+            }
+            else if(n[mid] > t){
+                r = mid-1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int [] res = new int [2];
+        res[0] = fres(nums,target);
+        res[1] = sres(nums,target);
         return res;
     }
 }
