4cea711eb49b1915aba6c2238a0913a7d1215864
rubanofficial <rubans082005@gmail.com>
2026-07-01 18:45:24 +0530

    Time: 7 ms (98.26%), Space: 65.1 MB (87.19%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    List<List<String>> res = new ArrayList<>();
+
+    public List<List<String>> partition(String s) {
+
+        int n = s.length();
+
+        // dp[i][j] = true if substring i..j is palindrome
+        boolean[][] dp = new boolean[n][n];
+
+        // Fill DP table
+        for (int len = 1; len <= n; len++) {
+
+            for (int i = 0; i + len - 1 < n; i++) {
+
+                int j = i + len - 1;
+
+                if (len == 1) {
+                    dp[i][j] = true;
+                }
+                else if (len == 2) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                }
+                else {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j))
+                            && dp[i + 1][j - 1];
+                }
+            }
+        }
+
+        backtrack(0, s, dp, new ArrayList<>());
+
+        return res;
+    }
+
+    private void backtrack(int start,String s,boolean[][] dp,List<String> path) {
+
+        if (start == s.length()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (dp[start][end]) {
+
+                path.add(s.substring(start, end + 1));
+
+                backtrack(end + 1, s, dp, path);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+}
